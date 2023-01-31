56173211f310a922b21724a9abf8425f32d73f81
Misha Solt <mikhail.us@gmail.com>
2023-01-31 16:50:16 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
-// import "./css/style.css"; //importing css
+import "./css/style.css"; //importing css
+
+const crypto = document.querySelector("#dropdown");
+const amount = document.querySelector("#inputAmount");
+const currency = document.querySelector("#dropdown2");
+const convert = document.querySelector('form');
+const output = document.querySelector('#output');
+
+async function converter(e) {
+    e.preventDefault();
+
+    try {
+      let response = await fetch(
+        `https://api.coinconvert.net/convert/${crypto.value}/${currency.value}?amount=${amount.value}`
+      );
+      let data = await response.json();
+  
+      console.log(data);
+  
+      output.value = Object.values(data)[2];
+  
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+convert.addEventListener("submit", converter);
+
+
+// version 2.0
+// const converter = () => {
+//     fetch(`https://api.coinconvert.net/convert/${crypto.value}/${currency.value}?amount=${amount.value}`)
+//     .then((response) => response.json())
+//     .then((data) => output.value = Object.values(data)[2])
+//     .catch((err) => console.log(err));
+// };
+
+// convert.addEventListener("submit", converter);
+
+
+
 
